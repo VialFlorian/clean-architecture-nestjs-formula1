4b4f92b9b436d0fcb832c5febaebfeb5e0ade3a0
Florian <13469873+VialFlorian@users.noreply.github.com>
2025-01-10 10:33:57 +0100

    chore: setup env file

--- src/infra/app/app.module.ts
+++ src/infra/app/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { APP_GUARD } from '@nestjs/core';
 import { DriverModule } from './driver/module';
 import { RolesGuard } from './roles.helper';
 
 @Module({
-  imports: [DriverModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env.dev',
+      isGlobal: true,
+    }),
+    DriverModule,
+  ],
   providers: [
     {
       provide: APP_GUARD,
